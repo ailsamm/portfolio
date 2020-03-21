ed724a0141d0da01a6740f49d07b2ab739a8b9c3
Ailsa Meechan-Maddon <ailsamm@outlook.com>
2020-03-21 12:47:27 -0700

    First version of pronunciation clip

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
+//let pron = new Audio("https://www.pronouncenames.com/mp3/3044_1242265006968.mp3"); 
 $('.progress-bar').css('width', '0');
+
+/* function playSound() {
+    $(".myButton").click(function() {
+        pron.play();
+    )}; */
 
 function showProgress() {
     $(window).on('load', function() {
